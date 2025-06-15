dd8329be50732fcfd1dc4f1400b59a52e97ed586
Nischal Soni <nischalsoni28@gmail.com>
2025-06-15 21:12:42 +0530

    Update mongourl

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://nischal:Nischal%402002@cluster0.abnfh.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-   
+    //await mongoose.connect('mongodb+srv://nischal:Nischal%402002@cluster0.abnfh.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://nischal:Nischal_2002@menu.qui0cgh.mongodb.net//food-del').then(()=>console.log("DB Connected"));
+   //mongodb+srv://nischal:Nischal_2002@menu.qui0cgh.mongodb.net/
 }
 
 
