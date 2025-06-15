8ae302ae75b569ede6870283fc2e0fc99b132bc3
Nischal Soni <nischalsoni28@gmail.com>
2025-06-15 22:21:46 +0530

    no change

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './Add.css'
-import { assets, url } from '../../assets/assets';
+import { assets, url } from '../../assets/assets'; 
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
